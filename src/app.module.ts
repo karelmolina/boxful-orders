a286390b7a899d5f7c5f17a83a187aab20b3285a
Molina, Karel <karelmzuniga@gmail.com>
2026-05-10 15:23:43 -0600

    feat: add dockerfile and cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
 import { DatabaseModule } from './database/database.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { DatabaseModule } from './database/database.module';
       envFilePath: '.env',
     }),
     DatabaseModule,
+    HealthModule,
     UsersModule,
     AuthModule,
     OrdersModule,
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import { setupSwagger } from './swagger.setup';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalGuards(new JwtAuthGuard(app.get(Reflector)));
   setupSwagger(app);
